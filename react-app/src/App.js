7ce08c8654a3343ceeae49adc16c8a14fcc49891
Arthur Martins <109447711+martins8@users.noreply.github.com>
2022-10-17 15:36:41 -0300

    Add files via upload

---
+++ react-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Aluno: Arthur Martins de Lima
+        </p>
+        <p>
+          Meu primeiro APP React, utilizando a estrutura base do create-react-app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
